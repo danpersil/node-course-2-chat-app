fced0aa3f13a55f13545c640e377b0e56e4f20a2
Daniel Pereira da Silva <AVA092@einstein.br>
2018-05-28 22:15:09 -0300

    Add files

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socketIO');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
-
 var app = express();
+const port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up on the port ${port}`);
 });
