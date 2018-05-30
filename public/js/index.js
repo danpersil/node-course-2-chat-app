d5c7bd550814b413e10694dc813874fcf52834c9
Daniel Pereira da Silva <AVA092@einstein.br>
2018-05-29 21:32:00 -0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
 var socket = io();
 socket.on('connect',function()  {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Daniel',
-    text: 'That .'
-  });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -14,25 +14,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log("New user connected");
 
-  socket.emit('newMessage', {
-    from: 'daniel.da.silva@avanade.com',
-    text: 'Hey, whats going on.',
-    createdAt: new Date()
-  });
-
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
-
-
-
 });
-
-
 
 server.listen(port, () => {
   console.log(`Server is up on the port ${port}`);
