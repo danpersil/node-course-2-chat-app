4316078a989c66611bdecc1ba1800194e93d862e
Daniel Pereira da Silva <AVA092@einstein.br>
2018-05-29 21:26:05 -0300

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,10 +14,22 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log("New user connected");
 
+  socket.emit('newMessage', {
+    from: 'daniel.da.silva@avanade.com',
+    text: 'Hey, whats going on.',
+    createdAt: new Date()
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
-  
+
+
+
 });
 
 
